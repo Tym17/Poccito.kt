a13002bebe441d4620192f377b012099f5cdecfb
Tym17 <Tym17@users.noreply.github.com>
2020-05-03 19:02:14 +0200

    to Kotlin

---
+++ desktop/src/com/tym17/poccito/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.tym17.poccito.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.tym17.poccito.poccito
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(poccito(), config)
+    }
+}
